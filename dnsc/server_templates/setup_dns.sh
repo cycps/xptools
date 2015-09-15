45a18e54ae51fca78cc6f4327fe99eeaeb09aaeb
Ryan Goodfellow <rgoodfel@isi.edu>
2015-09-14 19:33:54 -0700

    appeasing satan (apparmor)

--- dnsc/server_templates/setup_dns.sh
+++ dnsc/server_templates/setup_dns.sh
@@ -4,8 +4,8 @@ cp named.conf /etc/bind/
 cp named.conf.local /etc/bind
 cp named.conf.options /etc/bind
 cp keys.conf /etc/bind
-mkdir -p /etc/bind/zones
-cp db.{{.Xpname}}.cypress.net /etc/bind/zones/
+mkdir -p /var/cache/bind/zones
+cp db.{{.Xpname}}.cypress.net /var/cache/bind/zones
 cp usr.sbin.named /etc/apparmor.d/local/
 
 apparmor_parser -r /etc/apparmor.d/usr.sbin.named
