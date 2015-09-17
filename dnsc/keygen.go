d4a6395ff88d199c44c5ed72194ebddc0843a93b
Ryan Goodfellow <rgoodfel@isi.edu>
2015-09-16 20:39:19 -0700

    dnssec invocation fix

--- dnsc/keygen.go
+++ dnsc/keygen.go
@@ -5,7 +5,11 @@ import (
 )
 
 func Keygen(dnsname string) error {
-	cmd := exec.Command("dnssec-keygen", "-a", "HMAC-MD5", "-n", "USER", dnsname+".")
+	cmd := exec.Command(
+		"dnssec-keygen", "-r", "/dev/urandom",
+		"-a", "HMAC-MD5", "-b", "512",
+		"-n", "USER",
+		dnsname+".")
 	err := cmd.Run()
 	if err != nil {
 		return err
