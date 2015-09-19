af5d3a57718a8adf22d239f423c756d2712a7629
Ryan Goodfellow <rgoodfel@isi.edu>
2015-09-18 18:36:53 -0700

    working on scripts

--- routec/quagga_config/bootstrap.sh
+++ routec/quagga_config/bootstrap.sh
@@ -1,4 +1,8 @@
 #/usr/bin/env bash
 
+sudo apt-get update
 sudo apt-get install -y golang tmux quagga
 
+cd /tmp/cypress
+chmod +x do-quagga-config.sh
+./do-quagga-config.sh
--- routec/quagga_config/do-quagga-config.sh
+++ routec/quagga_config/do-quagga-config.sh
@@ -1,6 +1,9 @@
 #/usr/bin/env bash
 
-export GOPATH=/proj/cypress/go
-export PATH=$PATH:$GOPATH/bin
+#export GOPATH=/proj/cypress/go
+#export PATH=$PATH:$GOPATH/bin
+
+source /proj/cypress/go/env.sh
 
 quagga_config /proj/cypress/exp/{{.User}}-{{.XP}}/{{.XP}}.route/{{.Id}}.rc.json
+
